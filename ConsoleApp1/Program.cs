7485137d88eac658c30b73bf3f8a878cfc87a030
rMakarenkov <romgover@gmail.com>
2022-07-06 19:51:51 +0300

    Add my first programm

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = new int[] { -7, 1, 5, 2, -4, 3, 0 };
+            int leftSum;
+            int rightSum;
+            int temp = 0;
+            bool trust = true;
+            
+            do
+            {
+                for (int k = 1; k < Array.Length; k++)
+                {
+                    leftSum = 0;
+                    rightSum = 0;
+                    temp++;
+                    for (int i = 0; i < k; i++)
+                    {
+                        leftSum += Array[i];
+                    }
+                        for (int j = Array.Length - 1; j > k; j--)
+                    {
+                        rightSum += Array[j];
+                    }
+                    if (leftSum == rightSum)
+                    {
+                        trust = false;
+                        break;
+                    }
+                }
+            } while (trust == true) ;
+
+            Console.WriteLine("Элемент массива с равнозначной суммой левой и праввой частей массива = {0}", temp);
+            Console.ReadKey();
+        } 
+    }
+}
