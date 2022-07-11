0acfa48f8b56122168e3e6e8ac36196cfd45f20e
rMakarenkov <romgover@gmail.com>
2022-07-11 21:52:08 +0300

    Игра "Угадай число"

---
+++ Lesson2.Number/Lesson2.Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2.Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(-10,10);
+            int x;
+            Console.WriteLine("Угадайте число в диапазоне от -10 до 10");
+
+            do
+                {
+                Console.WriteLine("Введите число:");
+                x = Convert.ToInt32(Console.ReadLine());
+                if (value == x)
+                {
+                    Console.WriteLine("Поздравляю, вы угадали число");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Не угадали!");
+                }
+
+            } while (true);
+            Console.ReadKey();
+        }
+    }
+}
